f32b6e7d892ebe01e51eda7f91489605855cfa64
jankovicgd <jankovic.gd@gmail.com>
2017-07-19 15:53:50 +0200

    building backend

--- app/script.py
+++ app/script.py
@@ -2,12 +2,17 @@ from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['GET'])
 def index():
+    return render_template("index.html")
+
+@app.route("/success", methods = ['POST'])
+def success():
     if request.method == 'POST':
         file = request.files["files"]
         content = file.read()
-    return render_template("index.html")
+        print(file)
+        return render_template("success.html")
 
 if __name__ == '__main__':
     app.debug = True
