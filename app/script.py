6b8f80cdc249814957025b77d389228312b0eaa3
jankovicgd <jankovic.gd@gmail.com>
2017-07-20 11:53:25 +0200

    finish structuring

--- app/script.py
+++ app/script.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, send_file
+from geopy.geocoders import Nominatim
 import pandas
 
 app = Flask(__name__)
@@ -11,10 +12,21 @@ def index():
 def success():
     if request.method == 'POST':
         file = request.files["files"]
-        content = file.read()
-        print(file)
-        print(content)
-        return render_template("index.html")
+        try:
+            df = pandas.read_csv(file)
+            gc = Nominatim()
+            df["coordinates"] = df["Address"].apply(gc.geocode)
+            df["Latitude"] = df["coordinates"].apply(lambda x: x.latitude if x != None else None)
+            df["Longitude"] = df["coordinates"].apply(lambda x: x.longitude if x != None else None)
+            df = df.drop("coordinates", 1)
+            df.to_csv("uploads/geocoded.csv", index=None)
+            return render_template("index.html", text = df.to_html(), btn ="download.html")
+        except:
+            return render_template("index.html", text = "File not properly structured")
+
+@app.route("/download-file/")
+def download():
+    return send_file("uploads/geocoded.csv", attachment_filename='yourfile.csv', as_attachment=True)
 
 if __name__ == '__main__':
     app.debug = True
